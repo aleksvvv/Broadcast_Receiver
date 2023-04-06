5c96901d75beefab5f2102b3cc2e207835d65962
aleksvvv <adm-shop-001@yandex.ru>
2023-04-06 20:32:09 +0300

    All lesson

--- app/src/main/java/com/bignerdranch/android/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/broadcastreceiver/MainActivity.kt
@@ -1,38 +1,75 @@
 package com.bignerdranch.android.broadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private val localBroadcastReceiver by lazy {
+        LocalBroadcastManager.getInstance(this)
+    }
+
+    private lateinit var progressBar: ProgressBar
+    var page = 0
+//    private val receiver = MyReceiver()
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                "loaded" -> {
+                    val tt = intent.getIntExtra("percent", 0)
+                    progressBar.progress = tt
+                }
+                "Clicked" -> {
+
+                    var count = intent.getIntExtra("state", 0)
+                    Toast.makeText(
+                        context, "Clicked count - $count",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+        }
+    }
+
+
+    private val service = MyService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var page = 0
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val intent = Intent("Clicked")
-            intent.putExtra("state",page++)
-            sendBroadcast(intent)
-        }
+        progressBar = findViewById(R.id.progressBar)
 
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
 
         val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(Intent.ACTION_BATTERY_LOW)
             addAction("Clicked")
+            addAction("loaded")
         }
-
-        registerReceiver(receiver,intentFilter)
+        localBroadcastReceiver.registerReceiver(receiver, intentFilter)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Intent("Clicked").apply {
+                putExtra("state", page++)
+                localBroadcastReceiver.sendBroadcast(this)
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
+        localBroadcastReceiver.unregisterReceiver(receiver)
     }
 }
--- app/src/main/java/com/bignerdranch/android/broadcastreceiver/MyReceiver.kt
+++ app/src/main/java/com/bignerdranch/android/broadcastreceiver/MyReceiver.kt
@@ -9,6 +9,11 @@ class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
 
         when (intent?.action) {
+//            "loaded"->{
+//                val tt = intent.getIntExtra("percent",0)
+//                Toast.makeText(context, "percent $tt",
+//                    Toast.LENGTH_SHORT).show()
+//            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnOn = intent.getBooleanExtra("state",false)
                 Toast.makeText(context, "AIRPLANE_MODE_CHANGED. Turned - $turnOn",
@@ -17,10 +22,10 @@ class MyReceiver : BroadcastReceiver() {
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(context, "BATTERY_LOW", Toast.LENGTH_LONG).show()
             }
-            "Clicked" ->{
-                var count = intent.getIntExtra("state",0)
-                Toast.makeText(context, "Clicked count - $count", Toast.LENGTH_SHORT).show()
-            }
+//            "Clicked" ->{
+//                var count = intent.getIntExtra("state",0)
+//                Toast.makeText(context, "Clicked count - $count", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
 }
