c869447dc937874245e7f659c7746d77a922bba8
aleksvvv <adm-shop-001@yandex.ru>
2023-04-06 20:34:44 +0300

    All lesson

---
+++ app/src/main/java/com/bignerdranch/android/broadcastreceiver/MyService.kt
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.broadcastreceiver
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import kotlin.concurrent.thread
+
+class MyService : Service() {
+    private val localBroadcastReceiver by lazy {
+        LocalBroadcastManager.getInstance(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        thread {
+            for (i in 1..10) {
+                Thread.sleep(1000)
+                Intent("loaded").apply {
+                    putExtra("percent", i * 10)
+                    localBroadcastReceiver.sendBroadcast(this)
+                }
+            }
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+
+        return null
+    }
+}
